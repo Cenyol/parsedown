16b38f7374f3a96cad0c56eec4350e56408a7183
Cenyol <cenyol@163.com>
2017-06-10 14:15:04 +0800

    增加读取md文件的功能“；

--- test.php
+++ test.php
@@ -2,8 +2,15 @@
 	// 以下是两行简单的试用，更多tutorial请见：https://github.com/erusev/parsedown/wiki/Tutorial:-Get-Started
 	require_once "Parsedown.php";
 	$parsedown = new Parsedown();
-	echo $parsedown->text('##this is a h2 title'); 
-	echo $parsedown->text('Hit the __Parse__ button or `⌘ + Enter`'); 
+	// echo $parsedown->text('##this is a h2 title'); 
+	// echo $parsedown->text('Hit the __Parse__ button or `⌘ + Enter`'); 
+
+	
+	$mdFile = fopen("README.md", "r") or die("Unable to open file!");
+	$mdString = fread($mdFile,filesize("README.md"));
+	echo $parsedown->text($mdString); 
+	
+	fclose($mdFile);
 
 
 
